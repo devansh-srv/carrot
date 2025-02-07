51e9d3c7b9f1a0a517f26c90d7252fb7f0efcda2
Devansh Srivastava <devansh.srv@gmail.com>
2025-02-07 21:23:48 +0530

    fix: readiness API

--- src/server.c
+++ src/server.c
@@ -6,6 +6,7 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <poll.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -23,6 +24,7 @@ void error(const char *msg) { perror(msg); }
 void errno_msg(const char *msg) {
   fprintf(stderr, "errno: %d, error: %s", errno, msg);
 }
+
 void nonB(int fd) { // we need non blocking sockets
   errno = 0;
   int flags = fcntl(fd, F_GETFL, 0);
@@ -31,35 +33,35 @@ void nonB(int fd) { // we need non blocking sockets
     exit(0);
   }
   flags |= O_NONBLOCK;
-  errno = 0;
   (void)fcntl(fd, F_SETFL, flags);
   if (errno) {
     errno_msg("fnctl() set flag");
     exit(0);
   }
 }
+
 // for managing the state
 struct Conn {
   int fd;
   bool want_read;
   bool want_write;
   bool want_close;
-  int *incoming;
+  uint8_t *incoming;
   int incoming_size;
   int incoming_capacity;
-  int *outgoing;
+  uint8_t *outgoing;
   int outgoing_size;
   int outgoing_capacity;
 };
 
-void append_buf(int **incoming, int *data, size_t len, int *incoming_size,
-                int *incoming_capacity) {
+void append_buf(uint8_t **incoming, uint8_t *data, size_t len,
+                int *incoming_size, int *incoming_capacity) {
   // dynamically allocate memory
   int req = (*incoming_size) + len;
   if (req > (*incoming_capacity)) {
     int new_cap =
         ((*incoming_capacity) * 2 > req ? (*incoming_capacity) * 2 : req);
-    int *tmp = (int *)realloc(*incoming, new_cap * sizeof(int));
+    uint8_t *tmp = (uint8_t *)realloc(*incoming, new_cap * sizeof(uint8_t));
     if (tmp == NULL) {
       perror("realloc()");
       return;
@@ -67,25 +69,25 @@ void append_buf(int **incoming, int *data, size_t len, int *incoming_size,
     *incoming = tmp;
     (*incoming_capacity) = new_cap;
   }
-  memcpy(&(*incoming)[*incoming_size], data, len * sizeof(int));
+  memcpy(&(*incoming)[*incoming_size], data, len * sizeof(uint8_t));
   (*incoming_size) += len;
 }
 
-void consume_buf(int **incoming, size_t len, int *incoming_size,
+void consume_buf(uint8_t **incoming, size_t len, int *incoming_size,
                  int *incoming_capacity) {
   // dynamically de-allocate memory
   if (len >= (size_t)*incoming_size) {
     *incoming_size = 0;
     return;
   }
-  memmove(*incoming, *incoming + len, (*incoming_size - len) * sizeof(int));
+  memmove(*incoming, *incoming + len, (*incoming_size - len) * sizeof(uint8_t));
   *incoming_size -= len;
   // we can shrink if needed
   if (*incoming_size < *incoming_capacity / 4) {
     int new_cap = (*incoming_capacity) / 2;
     if (new_cap < 4)
       new_cap = 4;
-    int *tmp = (int *)realloc(*incoming, new_cap * sizeof(int));
+    uint8_t *tmp = (uint8_t *)realloc(*incoming, new_cap * sizeof(uint8_t));
     if (tmp) {
       *incoming = tmp;
       *incoming_capacity = new_cap;
@@ -93,91 +95,129 @@ void consume_buf(int **incoming, size_t len, int *incoming_size,
   }
 }
 
-int read_full(int sockfd, char *buf, size_t n) {
-  ssize_t rv;
-  while (n > 0) {
-    rv = read(sockfd, buf, n);
-    if (rv <= 0) {
-      return -1;
-    }
-    assert((size_t)rv <= n); // can read less than n bytes due to SIGINT
-    n -= (size_t)rv;
-    buf += rv; // advancing the pointer for next read cycle
+bool one_req(struct Conn *conn) {
+  if (conn->incoming_size < 4) {
+    return false;
   }
-  return 0;
-}
-
-int write_all(int sockfd, const char *buf, size_t n) {
-  ssize_t rv;
-  while (n > 0) {
-    rv = write(sockfd, buf, n);
-    if (rv <= 0) {
-      return -1;
-    }
-    assert((size_t)rv <= n);
-    n -= (size_t)rv;
-    buf += rv;
-  }
-  return 0;
-}
-
-/* poll(struct pollfd *fds, nfds_t nfds, int timeout) */
-int one_req(int sockfd) {
-
-  char rbuf[4 + MAX_PAYLOAD_SIZE];
-  int err = read_full(sockfd, rbuf, 4);
-  if (err) {
-    error("read()");
-    return err;
-  }
-
   uint32_t len = 0;
-  memcpy(&len, rbuf, 4);
-  len = ntohl(len); // change endianess
-  if (len > MAX_PAYLOAD_SIZE) {
+  memcpy(&len, conn->incoming, 4);
+  int host_len = ntohl(len);
+  if ((size_t)host_len > MAX_PAYLOAD_SIZE) {
     error("msg too long");
-    return -1;
+    conn->want_close = true;
+    return false;
   }
+  if (host_len + 4 > conn->incoming_size) {
+    return false;
+  }
+  uint8_t *req = &(conn->incoming[4]);
+  printf("client says: len:%d data:%.*s\n", host_len,
+         host_len < 100 ? host_len : 100, req);
 
-  err = read_full(sockfd, &rbuf[4], len);
-  if (err) {
-    error("read()");
-    return err;
-  }
-  /* fprintf(stderr, "client says: %.*s\n", len, &rbuf[4]); */
-  /* bzero(rbuf, sizeof(rbuf)); */
-  /* printf("Client says: %s", &rbuf[4]); */
-
-  /* len = sizeof(msg); */
-  /* if (len > MAX_PAYLOAD_SIZE) { */
-  /*   error("msg too long"); */
-  /*   return -1; */
-  /* } */
-  const char reply[] = "PONG";
-  char wbuf[4 + sizeof(reply)];
-  len = (uint32_t)strlen(reply);
-  uint32_t len_net = htonl(len);
-  memcpy(wbuf, &len_net, 4);
-  memcpy(&wbuf[4], reply, len);
-  err = write_all(sockfd, wbuf, 4 + len);
-  /* bzero(wbuf, sizeof(wbuf)); */
-  if (err) {
-    error("write_all()");
-    return -1;
-  }
-  return 0;
-  /* char wbuf[4 + MAX_PAYLOAD_SIZE]; */
-  /* memcpy(wbuf, &len, 4); */
-  /* memcpy(wbuf, msg, len); */
-  /* err = write_all(sockfd, wbuf, 4 + len); */
-  /* if (err) { */
-  /*   return err; */
-  /* } */
-  /* return 0; */
+  append_buf(&conn->outgoing, (uint8_t *)&len, 4, &conn->outgoing_size,
+             &conn->outgoing_capacity);
+  append_buf(&conn->outgoing, req, host_len, &conn->outgoing_size,
+             &conn->outgoing_capacity);
+  consume_buf(&conn->incoming, 4 + host_len, &conn->incoming_size,
+              &conn->incoming_capacity);
+  return true;
 }
+
+// callback for accept
+struct Conn *handle_accpet(int fd) {
+  struct sockaddr_in client;
+  socklen_t clilen = sizeof(client);
+  errno = 0;
+
+  int sockfd = accept(fd, (struct sockaddr *)&client, &clilen);
+  if (sockfd < 0) {
+    errno_msg("accept() fail 104");
+    return NULL;
+  }
+  uint32_t ip = client.sin_addr.s_addr;
+  fprintf(stderr, "client_addr: %d.%d.%d.%d:%d\n", ip & 255, (ip >> 8) & 255,
+          (ip >> 16) & 255, (ip >> 24), ntohs(client.sin_port));
+  nonB(sockfd);
+
+  struct Conn *conn = (struct Conn *)malloc(sizeof(struct Conn));
+  conn->fd = sockfd;
+  conn->want_read = true;
+  conn->want_write = false;
+  conn->want_close = false;
+  conn->incoming = (uint8_t *)malloc(8 * sizeof(uint8_t));
+  conn->outgoing = (uint8_t *)malloc(8 * sizeof(uint8_t));
+  conn->incoming_size = 0;
+  conn->outgoing_size = 0;
+  conn->incoming_capacity = 8;
+  conn->outgoing_capacity = 8;
+
+  // need to check malloc idiot
+  if (conn->incoming == NULL || conn->outgoing == NULL) {
+    error("memory not available for incoming and outgoing");
+    free(conn->incoming);
+    free(conn->outgoing);
+    close(sockfd);
+
+    return NULL;
+  }
+  return conn;
+}
+// callback for write
+void handle_write(struct Conn *conn) {
+  assert(conn->outgoing_size > 0);
+  ssize_t err = write(conn->fd, &conn->outgoing[0], conn->outgoing_size);
+  if (err <= 0 && errno == EAGAIN) {
+    return;
+  }
+  if (err < 0) {
+    error("write() error");
+    conn->want_close = true;
+    return;
+  }
+  consume_buf(&conn->outgoing, (size_t)err, &conn->outgoing_size,
+              &conn->outgoing_capacity);
+  if (conn->outgoing_size == 0) {
+    conn->want_write = false;
+    conn->want_read = true;
+  }
+}
+// callback for read
+void handle_read(struct Conn *conn) {
+  uint8_t buf[64 * 1024];
+  int err = read(conn->fd, buf, sizeof(buf));
+  if (err < 0 && errno == EAGAIN) {
+    return;
+  }
+  if (err < 0) {
+    error("read() error");
+    conn->want_close = true;
+    return;
+  }
+  if (err == 0) {
+    if (conn->incoming_size == 0) {
+      error("client disconnected");
+    } else {
+      error("EOF");
+    }
+    conn->want_close = true;
+    return;
+  }
+  append_buf(&conn->incoming, buf, (size_t)err, &conn->incoming_size,
+             &conn->incoming_capacity);
+
+  // isn't very clear
+  while (one_req(conn)) {
+  }
+  // wants to write
+  if (conn->outgoing_size > 0) {
+    conn->want_read = false;
+    conn->want_write = true;
+    return handle_write(conn);
+  }
+}
+
 int main(int argc, char *argv[]) {
   int fd;
-  socklen_t clilen;
   struct sockaddr_in server;
   bzero(&server, sizeof(server));
   if (argc < 2)
@@ -191,7 +231,7 @@ int main(int argc, char *argv[]) {
   server.sin_family = AF_INET;
   server.sin_addr.s_addr = INADDR_ANY;
   server.sin_port = htons(atoi(argv[1]));
-
+  nonB(fd);
   if (bind(fd, (const struct sockaddr *)&server, sizeof(server)) < 0) {
     error("bind() error");
     exit(0);
@@ -202,23 +242,102 @@ int main(int argc, char *argv[]) {
     exit(0);
   }
 
+  struct Conn **fdconn = (struct Conn **)malloc(8 * sizeof(struct Conn *));
+  if (fdconn == NULL) {
+    error("fdconn error");
+    exit(0);
+  }
+  size_t fdconn_cap = 8;
+  struct pollfd *poll_args = (struct pollfd *)malloc(8 * sizeof(struct pollfd));
+  if (poll_args == NULL) {
+    error("pollfd error");
+    exit(0);
+  }
+  int poll_args_size = 0;
+  int poll_args_cap = 8;
   while (1) {
-    struct sockaddr_in client;
-    clilen = sizeof(client);
-    int newsock = accept(fd, (struct sockaddr *)&client, &clilen);
-    if (newsock < 0)
-      error("accept()");
-    while (1) {
-      /* printf("Enter your message for the client: "); */
-      /* char msg[MAX_PAYLOAD_SIZE]; */
-      /* scanf("%s", msg); */
-      int err = one_req(newsock);
-      if (err) {
-        break;
+    poll_args_size = 0;
+    // 1 element for listening socket
+    struct pollfd pfd = {fd, POLL_IN, 0};
+    memcpy(&poll_args[poll_args_size], &pfd, sizeof(pfd));
+    poll_args_size += 1;
+    for (int i = 0; i < (int)fdconn_cap; i++) {
+      struct Conn *conn = fdconn[i];
+      if (!conn) {
+        continue;
+      }
+      struct pollfd pfd = {conn->fd, POLL_ERR, 0};
+      if (conn->want_read) {
+        pfd.events |= POLLIN;
+      }
+      if (conn->want_write) {
+        pfd.events |= POLLOUT;
+      }
+      int req = poll_args_size + sizeof(pfd);
+      if (req >= poll_args_cap) {
+        int new_cap = (2 * poll_args_cap) > req ? (2 * poll_args_cap) : req;
+        struct pollfd *tmp = (struct pollfd *)realloc(
+            poll_args, new_cap * sizeof(struct pollfd));
+        if (tmp == NULL) {
+          error("struct pollfd tmp 2 fail");
+          exit(0);
+        }
+        poll_args = tmp;
+        poll_args_cap = new_cap;
+      }
+      memcpy(&poll_args[poll_args_size], &pfd, sizeof(pfd));
+      poll_args_size += 1;
+    }
+    int err = poll(poll_args, (nfds_t)poll_args_size, -1);
+    if (err < 0 && errno == EAGAIN) {
+      continue;
+    }
+    if (err < 0) {
+      error("poll()");
+      exit(0);
+    }
+    if (poll_args[0].revents) {
+      struct Conn *conn = handle_accpet(fd);
+      if (conn) {
+        if (fdconn_cap < (size_t)conn->fd + 1) {
+          int new_cap = (size_t)conn->fd;
+          struct Conn **tmp = (struct Conn **)realloc(
+              fdconn, (size_t)new_cap * sizeof(**fdconn));
+          if (tmp == NULL) {
+            error("fdconn realloc()");
+            exit(0);
+          }
+          fdconn = tmp;
+          fdconn_cap = new_cap;
+        }
+        assert(!fdconn[conn->fd]);
+        fdconn[conn->fd] = conn;
       }
     }
-    close(newsock);
+    for (int i = 0; i < poll_args_cap; i++) {
+      short ready = poll_args[i].revents;
+      if (ready == 0) {
+        continue;
+      }
+      struct Conn *conn = fdconn[poll_args[i].fd];
+      if (ready & POLLIN) {
+        assert(conn != NULL);
+        assert(conn->want_read);
+        handle_read(conn);
+      }
+      if (ready & POLLOUT) {
+        assert(conn->want_write);
+        handle_write(conn);
+      }
+      if ((ready & POLLERR) || conn->want_close) {
+
+        close(conn->fd);
+        free(conn->incoming);
+        free(conn->outgoing);
+        fdconn[conn->fd] = NULL;
+        free(conn);
+      }
+    }
   }
-  close(fd);
   return 0;
 }
